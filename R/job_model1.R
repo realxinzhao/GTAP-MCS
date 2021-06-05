3dd6ae3ed14bbbe4041e08f0b20cd20e5c9313d1
Xin Zhao <xinzhao@umd.edu>
2021-06-05 12:36:48 -0400

    update out metric

--- R/job_model1.R
+++ R/job_model1.R
@@ -2,7 +2,7 @@ model <- "model1"
 
 #SCENS <- "mcet_default"
 
-SCENS <- paste0("mcet", seq(281,290))
+SCENS <- paste0("mcet", seq(501,550))
 
 for (scen in SCENS) {
   cmd_command <- paste0("gtap -cmf ../cmf/", scen, ".cmf -log ../log/", scen, ".log")
--- R/proddata.R
+++ R/proddata.R
@@ -44,34 +44,113 @@ for (id in c(1:nruns)) {
 }
 
 
-
-scen <- "mcet1"
+SCEN_MCET <- paste0("mcet", 1:10)
+SCEN_MCET <- "7USGRNA"
 ################################################
 #read data from output database
 
-DBnm <- paste0("gtap/outDB/", scen, ".upd")
-data = read_har(DBnm) 
-data %>% 
-  purrr::pluck("AREA") %>% 
-  as.data.frame() %>% 
-  gather(area, value) %>% 
-  separate(col = area, into = c("crop", "region"), sep = "\\.") %>% 
-  mutate(variable = "area") %>% bind_rows(
-    data%>% 
-      purrr::pluck("PRDN") %>% 
+Proc_outdata <- function(SCEN_MCET){
+  assertthat::assert_that(is.character(SCEN_MCET))
+  
+  lapply(SCEN_MCET, function(scen){
+  
+    DBnm <- paste0("gtap/outDB/", scen, ".upd")
+    data = read_har(DBnm) 
+    data %>% 
+      purrr::pluck("AREA") %>% 
       as.data.frame() %>% 
-      gather(area, value) %>% 
+      gather(area, value) %>% group_by(area) %>%  summarize(value = sum(value)) %>% 
       separate(col = area, into = c("crop", "region"), sep = "\\.") %>% 
-      mutate(variable = "prod")
-  ) %>% bind_rows(
-    data%>% 
-      purrr::pluck("COVR") %>% 
-      as.data.frame() %>% 
-      gather(area, value) %>% 
-      separate(col = area, into = c("crop", "region"), sep = "\\.") %>% 
-      mutate(variable = "cover")
-  ) %>% 
-  mutate(scenario = gsub(".upd", "", DBnm)) -> A
+      mutate(variable = "area") %>% bind_rows(
+      #   data%>% 
+      #     purrr::pluck("PRDN") %>% 
+      #     as.data.frame() %>% 
+      #     gather(area, value) %>% group_by(area) %>%  summarize(value = sum(value)) %>% 
+      #     separate(col = area, into = c("crop", "region"), sep = "\\.") %>% 
+      #     mutate(variable = "prod")
+      # ) %>% bind_rows(
+        data%>% 
+          purrr::pluck("COVR") %>% 
+          as.data.frame() %>% 
+          gather(area, value) %>% group_by(area) %>%  summarize(value = sum(value)) %>%  
+          separate(col = area, into = c("crop", "region"), sep = "\\.") %>% 
+          mutate(variable = "cover")
+      ) %>% 
+      mutate(scenario = scen)
+    
+  }) %>% bind_rows() 
+  }
+
+Proc_outdata("7USGRNA") -> OutDB_land_cet
+
+Proc_outdata(paste0("mcet", 1:320)) -> OutDB_land_mcet
+Proc_outdata(paste0("mcet", 321:450)) -> OutDB_land_mcet
+OutDB_land_mcet %>% 
+  left_join(OutDB_land_cet %>% mutate(scenario = "cet") %>% 
+              spread(scenario, value)) %>% 
+  transmute(scenario, variable, region, crop, mcet = value/1000, cet = cet/1000, unit = "kha") %>% 
+  group_by(scenario) %>% 
+  summarise(RMSE = mean((mcet - cet)^2)^0.5, 
+            AE = sum(abs(mcet - cet)), 
+            MAE = mean(abs(mcet - cet)) ) -> A
+A %>% arrange(RMSE) %>% 
+  write.csv("output/metric_321_450.csv") 
+
    
 ##############################################
+#Second round: around mcet172
+
+################################################
+#generate parameters
+# ET11, ET12, ETL5 = ETL2; ETL6 fixed
+nruns = 100
+id_start = 501
+
+runif(nruns, 0, 2) -> ET11_factor
+runif(nruns, 0, 2) -> ET12_factor
+runif(nruns, 0, 2) -> ETL2_factor
+data.frame(ET11_factor = ET11_factor, 
+           ET12_factor = ET12_factor, 
+           ETL2_factor = ETL2_factor, id = id_start:(id_start + nruns - 1)) -> trial_para
+write.csv(trial_para, "gtap/parameter/trial_para_round2.csv")
+#############
+#generate cmf and prm files
+Prmnm <- "gtap/parameter/par_mcet169.prm"
+Prmnm_default = read_har(Prmnm)[c("ET11", "ET12", "ETL2", "ETL5", "ETL6")]
+cmf_default <- readLines(file("gtap/cmf/mcet_default.cmf"))
+
+for (id in c(id_start:(id_start + nruns - 1))) {
+  
+  data = Prmnm_default
+  #update parameter
+  data$ET11 = data$ET11 * trial_para$ET11_factor[match(id, trial_para$id)]   
+  data$ET12 = data$ET12 * trial_para$ET12_factor[match(id, trial_para$id)]
+  data$ETL2 = data$ETL2 * trial_para$ETL2_factor[match(id, trial_para$id)]
+  data$ETL5 = data$ETL2 
+  data$ETL6 = data$ETL6 
+  
+  write_har(data, paste0("gtap/parameter/", "par_mcet", id, ".prm"))
+  
+  #update cmf
+  cmf = cmf_default
+  cmf[19] = paste0("Updated file gtapDATA = ../outDB/mcet", id, ".upd;")
+  cmf[20] = paste0("Solution file = mcet",id, ";")
+  cmf[22] = paste0("file gtapPARL = ../parameter/par_mcet", id, ".prm;")
+  
+  writeLines(cmf, file(paste0("gtap/cmf/mcet", id, ".cmf") ))
+  
+}
+
+Proc_outdata(paste0("mcet", 501)) -> OutDB_land_mcet
+OutDB_land_mcet %>% 
+  left_join(OutDB_land_cet %>% mutate(scenario = "cet") %>% 
+              spread(scenario, value)) %>% 
+  transmute(scenario, variable, region, crop, mcet = value/1000, cet = cet/1000, unit = "kha") %>% 
+  group_by(scenario) %>% 
+  summarise(RMSE = mean((mcet - cet)^2)^0.5, 
+            AE = sum(abs(mcet - cet)), 
+            MAE = mean(abs(mcet - cet)) ) -> A
+A %>% arrange(RMSE) %>% 
+  write.csv("output/metric_501_560.csv") 
+
 
